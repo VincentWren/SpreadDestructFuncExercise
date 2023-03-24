e7365872bdc9980ee5f0190daeec576409dc6b47
Vincent Wren <vincentwren@BCCSs-MacBook-Pro.local>
2023-03-24 11:16:56 -0500

    Complete

---
+++ SpreadDestructFuncExercise.js
@@ -0,0 +1,22 @@
+const jPDinos = [
+    `Velociraptors`,
+    `Tyrannosaurus Rex`,
+    `Dilophsuarus`
+];
+
+function seeDinos(v, ...d) {
+    console.log(v);
+    console.log(d);
+};
+seeDinos(`Velociraptors`, `Tyrannosaurus Rex`, `Dilophsaurus`);
+
+const jPDCharacters = [
+    alanGrant: 'Sam Neill',
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum",
+];
+
+function seeCharacters(sc => {ellieSattler}) {
+    console.log(ellieSattler);
+};
+seeCharacters(jPDCharacters);
